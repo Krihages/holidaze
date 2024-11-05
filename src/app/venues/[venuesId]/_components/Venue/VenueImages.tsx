2ce8c081217f9ecb1b9be00c5153180b1df4f7a6
Krihages <kristian.hageseter+github@gmail.com>
2024-11-05 13:58:10 +0100

    type change for img

--- src/app/venues/[venuesId]/_components/Venue/VenueImages.tsx
+++ src/app/venues/[venuesId]/_components/Venue/VenueImages.tsx
@@ -2,20 +2,21 @@
 
 import { Media } from "@/types/venue";
 import randomVenueUrl from "@/lib/helpers/randomVenueUrl";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import useCheckImage from "@/hooks/useCheckImage";
 import imageBlur from "@/images/image-blur.jpg";
 
 export default function VenueImages({ images }: { images: Media[] }) {
+  const newUrl = useCheckImage(images[0]?.url as string);
+
   if (images?.length === 0) {
     images[0] = {
-      url: randomVenueUrl().src,
+      url: randomVenueUrl() as StaticImageData,
       alt: "placeholder venue alt text",
     };
   }
 
   if (images?.length === 1) {
-    const newUrl = useCheckImage(images[0]?.url);
     return (
       <div className="relative w-full  overflow-hidden aspect-square max-h-[600px] max-w-7xl mx-auto">
         <Image
