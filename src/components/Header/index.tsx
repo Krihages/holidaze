655e32eb5e55be35bc40dae9e94aaeac4ca96a2c
Krihages <kristian.hageseter+github@gmail.com>
2024-11-09 18:52:35 +0100

    disabled navbar (unsure if will be needed)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,11 @@ import cookies from "@/lib/cookies";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
 import Logo from "./Logo";
-import Nav from "./Nav";
 import Searchbar from "./Searchbar";
 
 /**
  * Header component that renders the main header of the application.
- * Renders the Logo, Nav, and User-specific components (LoggedIn or LoggedOut)
+ * Renders the Logo and User-specific components (LoggedIn or LoggedOut)
  * Some of the logged in components also depends on if the user is a venue manager or customer
  * @returns {JSX.Element} The header component
  */
@@ -20,7 +19,7 @@ export default function Header(): JSX.Element {
       <div className="p-4 bg-primary-light text-primary-foreground">
         <div className="flex justify-between items-center w-full max-w-7xl mx-auto">
           <Logo />
-          <Nav />
+
           <div className="flex gap-4 items-center">
             {user ? (
               <LoggedIn
