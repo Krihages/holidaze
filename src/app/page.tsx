f0299331cec4a05f424082949f0a683c4aafa18d
Krihages <kristian.hageseter+github@gmail.com>
2024-11-09 18:49:18 +0100

    temporary disabled homepage (redirect to venues)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Explore from "./_components/Explore";
+/* import Explore from "./_components/Explore"; */
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/venues");
   return (
-    <>
+    /*     <>
       <Explore />
-    </>
+    </> */
+
+    <></>
   );
 }
