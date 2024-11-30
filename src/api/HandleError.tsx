338d5a465a1eb27e03df4d038cd4bca36f902428
Krihages <kristian.hageseter+github@gmail.com>
2024-11-30 11:54:40 +0100

    updated errorhandling on fetch

--- src/api/HandleError.tsx
+++ src/api/HandleError.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import Section from "@/components/Section";
+import { Button } from "@/components/ui/button";
+
 type Error = {
   message: string;
 };
 
 export default function HandleError(error: Error) {
-  return <div>{error.message ?? "Something went wrong"}</div>;
+  console.log("error", error);
+  return (
+    <Section>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <p className="text-muted-foreground">Oops... something went wrong</p>
+        <p className="text-2xl font-bold">
+          Error: {error.message ?? "Something went wrong"}
+        </p>
+        <Button onClick={() => window.location.reload()}>Reload page</Button>
+      </div>
+    </Section>
+  );
 }
